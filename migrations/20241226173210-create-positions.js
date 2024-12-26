a1c9c7686f0265aa267587a1c22b8c9e84798d2b
Ld36 <luizdavidx798@gmail.com>
2024-12-26 13:45:04 -0400

    primeiro commit com aplicação nova

---
+++ migrations/20241226173210-create-positions.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Positions',{
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primarykey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      salary: {
+        type: DataTypes.STRING,
+        allowNull: null,
+      },
+      created_at: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      uptated_at: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.droptable('Positions')
+  }
+};
